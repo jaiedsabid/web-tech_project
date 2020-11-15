72d56977a5a4a6a3ffef9386f0722f7ded6a21e7
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2020-11-15 18:37:07 +0600

    Added cust_message to view

---
+++ view/cust_message.php
@@ -0,0 +1,6 @@
+<div class="message-home">
+    <h3>Do you want to book sit for your favorite movies shows?</h4>
+    <h4>If yes then you are in the right place.</h5>
+    <p>Book sits for your favorite movies. There are lots of latest movies shows are coming.</p>
+    <p><a href="buyer.php">Click here</a> to choose and book sit for movie shows.</p>
+</div>
