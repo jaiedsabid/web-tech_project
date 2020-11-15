dde54212b449c38e75b2d5ba0ed4396b7f11ff7b
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2020-11-15 19:05:08 +0600

    Added profile to navbar and made few changes in profile

--- view/navbar.php
+++ view/navbar.php
@@ -1,5 +1,6 @@
-<ul>
+<ul style="list-style: none;">
     <li><a href="home.php">Home</a></li>
+    <li><a href="profile.php">Profile</a></li>
     <li><a href="admin.php">Admin Panel</a></li>
     <li><a href="seller.php">Employer Panel</a></li>
     <li><a href="buyer.php">Book Tickets</a></li>
--- view/profile.php
+++ view/profile.php
@@ -1,11 +1,30 @@
+<?php
+session_start(); 
+if(empty($_SESSION["username"])) 
+{
+header("Location: ../control/login.php"); // Redirecting To Home Page
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    <style>
+    .container {
+        border: 1px solid black;
+        width: 1020px;
+    }
+    </style>
 </head>
 <body>
+    <?php include("navbar.php"); ?>
     
+    <div class="container">
+    <label for="name">Name: </label><?php echo $_SESSION['fullname']; ?> <br>
+    </div>
 </body>
 </html>
