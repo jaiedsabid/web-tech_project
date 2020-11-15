1388cffbe334695011d5b724cf99ceb6b39fb15e
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2020-11-15 20:23:23 +0600

    Added support page for support stuff

---
+++ view/support.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); 
+if(empty($_SESSION["username"])) 
+{
+header("Location: ../control/login.php"); // Redirecting To Home Page
+}
+
+if($_SESSION['utype'] != "support") // Redirecting to home if user is not admin type
+{
+    header("location: home.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Support Panel</title>
+</head>
+<body>
+    <footer>
+        <?php include("footer.php"); ?>
+    </footer>
+</body>
+</html>
