3b4365b5cbf9759fd78038033c83e881dc7f70d3
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2020-12-08 12:45:00 +0600

    Added admin_message and change-pass control element

---
+++ control/change-pass.php
@@ -0,0 +1,55 @@
+<?php
+
+include('db.php');
+
+$result = '';
+
+if(isset($_POST['submit']))
+{
+    if(!empty($_POST['password']) && !empty($_POST['cpassword']))
+    {
+        if($_POST['password'] == $_POST['cpassword'])
+        {
+            if(strlen($_POST['password']) >= 8)
+            {
+                $con = new db();
+                $conObj = $con->OpenCon();
+                $result = $con->changePassword($conObj, $_SESSION['all_data']['username'], $_POST['password']);
+                $con->CloseCon($conObj);
+            }
+
+            else {
+                $result = '<div id="action-message">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col text-center"><span>* Password length must be 8 digit *</span></div>
+                                    </div>
+                                </div>
+                            </div>' ;
+            }
+        }
+
+        else {
+            $result = '<div id="action-message">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col text-center"><span>* Password do not match *</span></div>
+                                </div>
+                            </div>
+                        </div>' ;
+        }
+    }
+
+    else
+    {
+        $result = '<div id="action-message">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col text-center"><span>* Password and Confirm password field cannot be empty *</span></div>
+                            </div>
+                        </div>
+                    </div>' ;
+    }
+}
+
+?>
---
+++ view/admin_message.php
@@ -0,0 +1,5 @@
+<span style="font-weight: 500; text-align: center;">Admin has all the power to control other users.
+</span> <br>
+Admin Panel and support is accessable for admin.<br>
+Use your power to make chnages for good.<br>
+<a href="admin.php">Click here</a> to go to Admin Panel.
